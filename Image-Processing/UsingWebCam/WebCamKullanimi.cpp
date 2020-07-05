ac4ed99c3a48d4686427e49cd5512fb542938372
akdedeimtisal <46938621+akdedeimtisal@users.noreply.github.com>
2020-07-05 14:56:41 +0300

    Update WebCamKullanimi.cpp

--- Image-Processing/UsingWebCam/WebCamKullanimi.cpp
+++ Image-Processing/UsingWebCam/WebCamKullanimi.cpp
@@ -1,5 +1,4 @@
-﻿// BosGoruntuOlusturma.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
+//WebCam'den görüntü alıp frame'de gösterir.
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
@@ -10,18 +9,18 @@ using namespace cv;
 
 int main()
 {
-	VideoCapture vid(0);
+	VideoCapture vid(0); //videcapture sınıfından bir vid değişkeni oluştururuz eğer içine 0 yazarsak bilgisayarın kendi webcamini, 1 yazarsak ilk harici webcami algılar.
 	if (!vid.isOpened()){
 		cout << "kamera yüklenemedi" << endl;
 		system("pause");
 		return -1;
 	}
-	vid.set(CAP_PROP_POS_MSEC, 300);
+	vid.set(CAP_PROP_POS_MSEC, 300); //300 milisaniye sonra işlemeye başlar.
 	namedWindow("webcam", 300);
 
 	while (1) {
 		Mat frame;
-		bool kont = vid.read(frame);
+		bool kont = vid.read(frame); //frame'in içine aldığımız görüntüyü yerleştiriyoruz.
 		if (!kont) {
 			cout << "webcam frame yüklenemedi" << endl;
 			system("pause");
