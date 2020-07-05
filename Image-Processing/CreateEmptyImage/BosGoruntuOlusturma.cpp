f91b326e0079fa5ed2df867ca0a717306ae0ec1b
akdedeimtisal <46938621+akdedeimtisal@users.noreply.github.com>
2020-07-05 14:26:32 +0300

    Update BosGoruntuOlusturma.cpp

--- Image-Processing/CreateEmptyImage/BosGoruntuOlusturma.cpp
+++ Image-Processing/CreateEmptyImage/BosGoruntuOlusturma.cpp
@@ -10,8 +10,8 @@ using namespace cv;
 
 int main()
 {
-    Mat img(480, 640, CV_8UC3, Scalar(255, 255, 0));
-
+    Mat img(480, 640, CV_8UC3, Scalar(255, 255, 0)); //480 satır 640 sütun,  8UC3 = 8 bitlik Unsigned (görseller pozitif olduğu için negatif değere ihtiyacımız yok o yüzden unsigned)
+                                                     //C3 = 3 Renk olduğunu (Red Green Blue) gösterir, scalar ile de renkgini ayarlıyoruz. 0 en koyu 255 en açık.
     if (img.empty()) {
         cout << "görsel yüklenemedi" << endl;
         system("pause");
