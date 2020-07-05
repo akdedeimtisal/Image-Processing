1a9366cd131a923909632f2f221acbdef056a398
akdedeimtisal <46938621+akdedeimtisal@users.noreply.github.com>
2020-07-05 14:23:53 +0300

    added

---
+++ Image-Processing/CreateEmptyImage/BosGoruntuOlusturma.cpp
@@ -0,0 +1,28 @@
+﻿// BosGoruntuOlusturma.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+    Mat img(480, 640, CV_8UC3, Scalar(255, 255, 0));
+
+    if (img.empty()) {
+        cout << "görsel yüklenemedi" << endl;
+        system("pause");
+        return -1;
+    }
+    namedWindow("deneme", 100);
+    imshow("deneme", img);
+    waitKey(0);
+    destroyWindow("deneme");
+    return 0;
+
+
+
+}
---
+++ Image-Processing/Open an Image on Window/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+int main()
+{
+    Mat img = imread("C:/Users/ASUS/Desktop/akinci.jpg");
+    if (img.empty()) {
+        cout << "görsel yüklenmedi" << endl;
+        system("pause");
+        return -1;
+    }
+    namedWindow("deneme", 40);
+    imshow("deneme", img);
+    waitKey(0);
+    destroyWindow("deneme");
+    return 0;
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
